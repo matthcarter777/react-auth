86afcc09c0c47d985f55d6c5c44c01afea478139
Mateus <mateushbsb@gmail.com>
2022-05-25 20:19:14 -0300

    IGNITE - 008: user can component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
+import { Can } from "../components/Can";
 import { useAuth } from "../hooks/useAuth"
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
@@ -8,13 +10,25 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
   const { user } = useAuth();
 
+  const useCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  });
+
   useEffect(() => {
     api.get('/me')
     .then(response => console.log(response))
     .catch(err => console.error(err))
   }, [])
 
-  return <h1>H3y Y0u, {user?.email}</h1>
+  return(
+    <>
+      <h1>H3y Y0u, {user?.email}</h1>
+
+      <Can permissions={['metrics.list']}>
+        <div>Metricas</div>
+      </Can>
+    </>
+  ) 
 }
 
 export const getServerSideProps = withSSRAuth(async(ctx) => {
